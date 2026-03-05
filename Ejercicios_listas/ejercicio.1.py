c187fac02c49dfe73f72523b5800509c942ff14e
yulianasanta <yuliana.santa@utp.edu.co>
2026-03-04 20:01:52 -0500

    Add files via upload

--- Ejercicios_listas/ejercicio.1.py
+++ Ejercicios_listas/ejercicio.1.py
@@ -1,4 +1,7 @@
-# Una empresa de energía quiere llevar un registro de sus clientes y su consumo de energía. Se pide realizar un programa que permita ingresar el nombre del  cliente y su consumo en kWh, y luego calcular el costo total a pagar considerando que el precio por kWh es de $0.15.
+# Una empresa de energía quiere llevar un registro de sus clientes y su consumo de energía. 
+# Se pide realizar un programa que permita ingresar el nombre del  cliente y su consumo en kWh, 
+# y luego calcular el costo total a pagar considerando que el precio por kWh es de $0.15.
+
 lista = ["juan", "pablo", "maria", "hector", "luisa"]
 nombre = input ("ingrese su nombre en minuscula:")
 
@@ -8,5 +11,3 @@ else:
     lista.append (nombre)
     print ("nombre agregado a la lista")
     print (lista)
-    
-    
